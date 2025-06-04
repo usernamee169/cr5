4baed76077e2d2b92b72c7d9153846e9d8e2680b
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-04 19:03:34 +0500

    Update Point2D.java

--- lab3-main/3/src/ru/sergeeva/geometry/Point2D.java
+++ lab3-main/3/src/ru/sergeeva/geometry/Point2D.java
@@ -1,26 +1,26 @@
 package ru.sergeeva.geometry;
 
-
 /**
- * Класс, представляющий двухмерную точку с координатами X и Y.
+ * Двухмерная точка с координатами X и Y.
+ * @param x координата X (int)
+ * @param y координата Y (int)
+ * @param color цвет точки (String)
+ * @param creationTime время создания (String)
  */
 public class Point2D extends AbstractPoint {
     private int x;
     private int y;
 
-    // Конструктор для двухмерной точки с цветом и временем
     public Point2D(int x, int y, String color, String creationTime) {
         super(color, creationTime);
         this.x = x;
         this.y = y;
     }
 
-    // Конструктор для двухмерной точки без дополнительных характеристик
     public Point2D(int x, int y) {
         this(x, y, "не указан", "не указано");
     }
 
-    // Геттеры и сеттеры
     public int getX() { return x; }
     public void setX(int x) { this.x = x; }
 
