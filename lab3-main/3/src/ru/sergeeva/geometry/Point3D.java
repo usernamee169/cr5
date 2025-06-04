40dc6e42716e27aa0f53a1666edc97a14f35d89a
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-04 19:03:50 +0500

    Update Point3D.java

--- lab3-main/3/src/ru/sergeeva/geometry/Point3D.java
+++ lab3-main/3/src/ru/sergeeva/geometry/Point3D.java
@@ -1,15 +1,18 @@
 package ru.sergeeva.geometry;
 
-
 /**
- * Класс, представляющий трехмерную точку с координатами X, Y и Z.
+ * Трехмерная точка с координатами X, Y и Z.
+ * @param x координата X (int)
+ * @param y координата Y (int)
+ * @param z координата Z (int)
+ * @param color цвет точки (String)
+ * @param creationTime время создания (String)
  */
 public class Point3D extends AbstractPoint {
     private int x;
     private int y;
     private int z;
 
-    // Конструктор для трехмерной точки с цветом и временем
     public Point3D(int x, int y, int z, String color, String creationTime) {
         super(color, creationTime);
         this.x = x;
@@ -17,12 +20,10 @@ public class Point3D extends AbstractPoint {
         this.z = z;
     }
 
-    // Конструктор для трехмерной точки без дополнительных характеристик
     public Point3D(int x, int y, int z) {
         this(x, y, z, "не указан", "не указано");
     }
 
-    // Геттеры и сеттеры
     public int getX() { return x; }
     public void setX(int x) { this.x = x; }
 
