23edd8de032c36051d701c1293be233039de1ac8
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-04 19:04:10 +0500

    Update Polyline.java

--- lab3-main/3/src/ru/sergeeva/geometry/Polyline.java
+++ lab3-main/3/src/ru/sergeeva/geometry/Polyline.java
@@ -1,28 +1,36 @@
 package ru.sergeeva.geometry;
 
-
 /**
- * Класс, представляющий ломаную линию, заданную массивом точек.
+ * Класс для представления ломаной линии.
+ * @param points массив точек, образующих ломаную (Point[])
  */
 public class Polyline {
     private Point[] points;
 
-    // Конструктор
     public Polyline(Point[] points) {
         this.points = points.clone();
     }
 
-    // Метод для вычисления длины ломаной
+    /**
+     * Вычисление длины ломаной.
+     * @return длина ломаной (double)
+     */
     public double getLength() {
         double length = 0;
         for (int i = 0; i < points.length - 1; i++) {
-            length += Math.sqrt(Math.pow(points[i+1].getX() - points[i].getX(), 2) +
-                    Math.pow(points[i+1].getY() - points[i].getY(), 2));
+            int dx = points[i+1].getX() - points[i].getX();
+            int dy = points[i+1].getY() - points[i].getY();
+            length += Math.sqrt(dx*dx + dy*dy);
         }
         return length;
     }
 
-    // Метод для изменения точки в ломаной
+    /**
+     * Изменение точки в ломаной.
+     * @param index индекс изменяемой точки
+     * @param point новая точка
+     * @throws RuntimeException если индекс вне границ
+     */
     public void setPoint(int index, Point point) {
         if (index < 0 || index >= points.length) {
             throw new RuntimeException("Индекс за пределами массива");
