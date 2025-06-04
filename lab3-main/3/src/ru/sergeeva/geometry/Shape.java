5740dc7bcb45f519dd2a35a0cb286fe48274ff6f
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-04 20:58:04 +0500

    Update Shape.java

--- lab3-main/3/src/ru/sergeeva/geometry/Shape.java
+++ lab3-main/3/src/ru/sergeeva/geometry/Shape.java
@@ -1,22 +1,55 @@
 package ru.sergeeva.geometry;
 
 /**
- * Абстрактный класс для фигур.
+ * Абстрактный класс для представления геометрических фигур.
+ * @param color цвет фигуры (String)
+ * @param creationTime время создания фигуры (String)
  */
 public abstract class Shape {
     private String color;
     private String creationTime;
 
+    /**
+     * Конструктор для создания фигуры с цветом и временем создания.
+     * @param color цвет фигуры
+     * @param creationTime время создания
+     */
     public Shape(String color, String creationTime) {
         this.color = color;
         this.creationTime = creationTime;
     }
 
-    public String getColor() { return color; }
-    public void setColor(String color) { this.color = color; }
+    /**
+     * Получение цвета фигуры.
+     * @return цвет фигуры (String)
+     */
+    public String getColor() { 
+        return color; 
+    }
 
-    public String getCreationTime() { return creationTime; }
-    public void setCreationTime(String creationTime) { this.creationTime = creationTime; }
+    /**
+     * Установка цвета фигуры.
+     * @param color новый цвет фигуры
+     */
+    public void setColor(String color) { 
+        this.color = color; 
+    }
+
+    /**
+     * Получение времени создания фигуры.
+     * @return время создания (String)
+     */
+    public String getCreationTime() { 
+        return creationTime; 
+    }
+
+    /**
+     * Установка времени создания фигуры.
+     * @param creationTime новое время создания
+     */
+    public void setCreationTime(String creationTime) { 
+        this.creationTime = creationTime; 
+    }
 
     @Override
     public String toString() {
