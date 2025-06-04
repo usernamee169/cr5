9757ca78e464d153eb7ea6c588c8c6b609e346a5
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-04 19:03:21 +0500

    Update Point1D.java

--- lab3-main/3/src/ru/sergeeva/geometry/Point1D.java
+++ lab3-main/3/src/ru/sergeeva/geometry/Point1D.java
@@ -1,23 +1,23 @@
 package ru.sergeeva.geometry;
 
 /**
- * Класс, представляющий одномерную точку с координатой X.
+ * Одномерная точка с координатой X.
+ * @param x координата X (int)
+ * @param color цвет точки (String)
+ * @param creationTime время создания (String)
  */
 public class Point1D extends AbstractPoint {
     private int x;
 
-    // Конструктор для одномерной точки с цветом и временем
     public Point1D(int x, String color, String creationTime) {
         super(color, creationTime);
         this.x = x;
     }
 
-    // Конструктор для одномерной точки без дополнительных характеристик
     public Point1D(int x) {
         this(x, "не указан", "не указано");
     }
 
-    // Геттеры и сеттеры
     public int getX() { return x; }
     public void setX(int x) { this.x = x; }
 
