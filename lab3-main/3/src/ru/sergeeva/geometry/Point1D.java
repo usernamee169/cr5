94eb3cef9f4c6c2c60c3a1a727f4af3555f8a443
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-05 18:20:05 +0500

    Update Point1D.java

--- lab3-main/3/src/ru/sergeeva/geometry/Point1D.java
+++ lab3-main/3/src/ru/sergeeva/geometry/Point1D.java
@@ -18,8 +18,12 @@ public class Point1D extends AbstractPoint {
         this(x, "не указан", "не указано");
     }
 
-    public int getX() { return x; }
-    public void setX(int x) { this.x = x; }
+    public int getX() {
+        return x; 
+    }
+    public void setX(int x) { 
+        this.x = x; 
+    }
 
     @Override
     public String toString() {
