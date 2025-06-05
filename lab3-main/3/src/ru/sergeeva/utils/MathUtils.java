0809ee42fe6b8fc33ffcbc68c4bd6d5202c87df1
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-05 18:13:34 +0500

    Update MathUtils.java

--- lab3-main/3/src/ru/sergeeva/utils/MathUtils.java
+++ lab3-main/3/src/ru/sergeeva/utils/MathUtils.java
@@ -1,15 +1,15 @@
 package ru.sergeeva.utils;
 
-/**
- * Утилиты для математических операций.
- */
 public class MathUtils {
-    /**
+    /*
+     * Утилиты для математических операций.
+     */
+    public static double sum(double... values) {
+    /*
      * Суммирование чисел.
      * @param values числа для суммирования (double...)
      * @return сумма чисел (double)
      */
-    public static double sum(double... values) {
         double sum = 0;
         for (double value : values) {
             sum += value;
