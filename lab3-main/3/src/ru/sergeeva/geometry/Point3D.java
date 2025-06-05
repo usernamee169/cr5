b95c7ec6713764022ebd93c27f71292933304978
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-05 18:21:51 +0500

    Update Point3D.java

--- lab3-main/3/src/ru/sergeeva/geometry/Point3D.java
+++ lab3-main/3/src/ru/sergeeva/geometry/Point3D.java
@@ -24,14 +24,29 @@ public class Point3D extends AbstractPoint {
         this(x, y, z, "не указан", "не указано");
     }
 
-    public int getX() { return x; }
-    public void setX(int x) { this.x = x; }
+    public int getX() { 
+        return x; 
+    }
+    
+    public void setX(int x) { 
+        this.x = x; 
+    }
 
-    public int getY() { return y; }
-    public void setY(int y) { this.y = y; }
+    public int getY() { 
+        return y;
+    }
+    
+    public void setY(int y) { 
+        this.y = y; 
+    }
 
-    public int getZ() { return z; }
-    public void setZ(int z) { this.z = z; }
+    public int getZ() {
+        return z; 
+    }
+    
+    public void setZ(int z) { 
+        this.z = z;
+    }
 
     @Override
     public String toString() {
