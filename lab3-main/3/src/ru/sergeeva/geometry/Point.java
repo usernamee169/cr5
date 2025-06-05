6f274783da18c385b98e700c81b6d60268fadaf6
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-05 18:19:36 +0500

    Update Point.java

--- lab3-main/3/src/ru/sergeeva/geometry/Point.java
+++ lab3-main/3/src/ru/sergeeva/geometry/Point.java
@@ -29,14 +29,26 @@ public class Point {
         this.z = z;
     }
 
-    public int getX() { return x; }
-    public void setX(int x) { this.x = x; }
+    public int getX() { 
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x; 
+    }
 
-    public int getY() { return y; }
-    public void setY(int y) { this.y = y; }
+    public int getY() { 
+        return y; 
+    }
+    public void setY(int y) {
+        this.y = y; 
+    }
 
-    public int getZ() { return z; }
-    public void setZ(int z) { this.z = z; }
+    public int getZ() {
+        return z;
+    }
+    public void setZ(int z) {
+        this.z = z; 
+    }
 
     @Override
     public String toString() {
