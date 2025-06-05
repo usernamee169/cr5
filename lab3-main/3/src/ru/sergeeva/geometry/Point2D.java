4c96f0b88c05405b3d60d1d1b6332473ef764036
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-05 18:20:56 +0500

    Update Point2D.java

--- lab3-main/3/src/ru/sergeeva/geometry/Point2D.java
+++ lab3-main/3/src/ru/sergeeva/geometry/Point2D.java
@@ -21,11 +21,21 @@ public class Point2D extends AbstractPoint {
         this(x, y, "не указан", "не указано");
     }
 
-    public int getX() { return x; }
-    public void setX(int x) { this.x = x; }
+    public int getX() { 
+        return x; 
+    }
+    
+    public void setX(int x) { 
+        this.x = x; 
+    }
 
-    public int getY() { return y; }
-    public void setY(int y) { this.y = y; }
+    public int getY() {
+        return y; 
+    }
+    
+    public void setY(int y) {
+        this.y = y; 
+    }
 
     @Override
     public String toString() {
